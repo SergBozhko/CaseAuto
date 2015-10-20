1795a312ff504c94567daf9f02cb48eaa26ddaee
Admin <admin@Mac-mini-FrontEnd.local>
2015-10-20 10:36:49 +0300

    star mobile ver

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(function() {
+	console.log('DOM ready . . .');
+
+	var $menuButton = $('#mobile-menu-toggle'),
+		$mobileNavModal = $('.mobile-navigation-modal'),
+		$mobileOverlay = $('.mobile-menu-overlay');
+
+	$menuButton.click(function(e) {
+		e.preventDefault();
+		console.log('Menu button clicked');
+		$mobileNavModal.toggleClass('mobile-navigation-modal-active');
+		$mobileOverlay.toggleClass('mobile-menu-overlay-active');
+	});
+	$mobileOverlay.click(function() {
+		console.log('Closed with overlay');
+		$mobileNavModal.toggleClass('mobile-navigation-modal-active');
+		$mobileOverlay.toggleClass('mobile-menu-overlay-active');
+	});
+});
